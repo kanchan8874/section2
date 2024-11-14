29b8fed8707f9a8ae64554a4989352db4724388a
m-triple-m <kanchankushwaha65520@gmail.com>
2024-11-14 16:11:37 +0530

    manageproduct

--- frontend/src/app/manageproduct/page.jsx
+++ frontend/src/app/manageproduct/page.jsx
@@ -6,30 +6,30 @@ import React, { useEffect } from 'react'
 import { useState } from 'react';
 import toast from 'react-hot-toast';
 
-const ManageUser = () => {
-    const[userList, setUserList] = useState([]);
+const ManageProduct = () => {
+    const[userProduct, setProductList] = useState([]);
     
 
 
-    const fetchUsers =()=>{
-        axios.get('http://localhost:5000/user/getall')
+    const fetchProduct =()=>{
+        axios.get('http://localhost:5000/product/getall')
         .then((result) =>{
             console.table(result.data);
-            setUserList(result.data);
+            setProductList(result.data);
         }).catch((err) =>{
             console.log(err);
         })
     }
     //hoks menagelife cycle of components example useeffect(),usestate()
     useEffect(() => {
-        fetchUsers();
+        fetchProduct();
     }, [])
 
-    const deleteUser = (id) =>{
-        axios.delete('http://localhost:5000/user/delete/'+id)
+    const deleteProduct = (id) =>{
+        axios.delete('http://localhost:5000/product/delete/'+id)
         .then((result) =>{
             toast.success('user deleted successfully');
-            fetchUsers();
+            fetchProduct();
         }).catch((err) =>{
             console.log(err);
             toast.error('failed to deleted user');
@@ -44,27 +44,27 @@ const ManageUser = () => {
                 <thead>
                     <tr className='bg-gray-600 text-white font-bold'>
                         <th className='p-3'>ID</th>
-                        <th className='p-3'>Name</th>
-                        <th className='p-3'>Email</th>
-                        <th className='p-3'>Password</th>
-                        <th className='p-3'>City</th>
+                        <th className='p-3'>NAME</th>
+                        <th className='p-3'>PRICE</th>
+                        <th className='p-3'>Description</th>
+                        <th className='p-3'>Category</th>
                         <th className='p-3'>Created At</th>
                         <th className='p-3' colSpan={2}></th>
                     </tr>
                 </thead>
                 <tbody>
                     {
-                        userList.map( (user) => { 
+                        userProduct.map( (user) => { 
                             return(
                                 <tr className='border bg-gray-200'>
                                     <td className='p-3'>{user._id} </td>
                                     <td className='p-3'>{user.name} </td>
-                                    <td className='p-3'>{user.email} </td>
-                                    <td className='p-3'>{user.password} </td>
-                                    <td className='p-3'>{user.city} </td>
+                                    <td className='p-3'>{user.price} </td>
+                                    <td className='p-3'>{user.description} </td>
+                                    <td className='p-3'>{user.category} </td>
                                     <td className='p-3'>{new Date(user.createdAt).toDateString()}</td>  
                                     <td className='p-3'>
-                                        <button onClick={() => { deleteUser(user._id) }}
+                                        <button onClick={() => { deleteProduct(user._id) }}
                                          className='bg-red-500 py-1 px-3 text-white rounded-full'>Delete</button>
                                     </td>
                                     <td className='p-3'>
@@ -86,4 +86,4 @@ const ManageUser = () => {
   )
 }
 
-export default ManageUser;
+export default ManageProduct;
